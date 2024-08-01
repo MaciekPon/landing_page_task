36acbaef2d85b00536bd9f3f99e7a385cfae2e8f
MaciejP <m.ponicki1995@gmail.com>
2024-08-01 21:14:45 +0200

    fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "landing_page_task",
+  publicPath: "/landing_page_task/",
 });
